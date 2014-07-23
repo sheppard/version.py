3d27dbf2143259540d7192344fe5adc81e725c3f
S. Andrew Sheppard <andrew@wq.io>
2014-07-23 17:21:41 -0500

    initial version

---
+++ setup.py
@@ -0,0 +1,44 @@
+import os
+from setuptools import setup
+
+LONG_DESCRIPTION = """
+Make an image containing a version number (or other text)
+"""
+
+
+def parse_markdown_readme():
+    """
+    Convert README.md to RST via pandoc, and load into memory
+    (fallback to LONG_DESCRIPTION on failure)
+    """
+    # Attempt to run pandoc on markdown file
+    import subprocess
+    try:
+        subprocess.call(
+            ['pandoc', '-t', 'rst', '-o', 'README.rst', 'README.md']
+        )
+    except OSError:
+        return LONG_DESCRIPTION
+
+    # Attempt to load output
+    try:
+        readme = open(os.path.join(
+            os.path.dirname(__file__),
+            'README.rst'
+        ))
+    except IOError:
+        return LONG_DESCRIPTION
+    return readme.read()
+
+setup(
+    name='version.py',
+    version='0.0.1',
+    author='S. Andrew Sheppard',
+    author_email='andrew@wq.io',
+    url='http://github.com/sheppard/version.py',
+    license='MIT',
+    scripts=['version.py'],
+    description=LONG_DESCRIPTION.strip(),
+    long_description=parse_markdown_readme(),
+    install_requires=['Pillow'],
+)
---
+++ version.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from PIL import Image, ImageDraw
+import sys
+
+USAGE = """
+Usage: version.py file.txt file.png [version]
+
+If no version is given it will be read from file.txt.
+If a version is given it will be written to file.txt.
+In either case version will be drawn to file.png.
+"""
+
+def run(text_path, image_path, version=None):
+    if version is None:
+        version = open(text_path).read().strip()
+    else:
+        open(text_path, 'w').write(version)
+    width = len(version) * 6 + 4 + 4
+    img = Image.new("RGB", (width, 16), color=(255, 255, 255))
+    draw = ImageDraw.Draw(img)
+    draw.text((4, 2), version, (0, 0, 0))
+    img.save(image_path)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print USAGE.strip()
+        exit()
+    run(*sys.argv[1:])
